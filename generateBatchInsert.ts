ef9e2f3896c12e1719f36c996564d201ce133738
Jakob Edelstein <jakobedelstein@gmail.com>
2026-02-01 20:15:06 -0500

    Pass filename arg to script

---
+++ generateBatchInsert.ts
@@ -0,0 +1,36 @@
+import fs from "node:fs/promises"
+
+if (process.argv.length < 3) {
+  console.log("No filename argument provided!");
+  process.exit(1);
+}
+
+const filename = process.argv[2];
+const jsonString = await fs.readFile(filename, 'utf-8')
+
+interface Transaction {
+  date: string,
+  category: string
+  merchant: string,
+  amount: number
+
+}
+
+const transactions: Transaction[] = JSON.parse(jsonString)
+
+const values = transactions.map(t => 
+	`(\'${t.date}\', \'${t.merchant}\', ${t.amount}, \'${t.category}\')`)
+	.join(",\n");
+
+const sql = `
+INSERT INTO credit_card_transaction (
+    transaction_date,
+    merchant_name,
+    transaction_amount,
+    transaction_category
+)
+VALUES
+${values};
+`;
+
+console.log(sql);
