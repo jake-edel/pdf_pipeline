7a487debef17f2239e48c9c62a9f425eaa2f8247
Jakob Edelstein <jakobedelstein@gmail.com>
2026-09-24 22:24:17 -0500

    Update scratchpad

--- src/scratchpad.ts
+++ src/scratchpad.ts
@@ -1,6 +1,9 @@
 import fs from "node:fs";
+import process from "node:process";
+import readline from "node:readline/promises";
 import { buildModel } from "./modules/category/buildModel.ts";
 import type { Transaction } from "./modules/transaction.ts";
+import { classify } from "./modules/category/classify.ts";
 
 const allTransactions: Transaction[] = [];
 const transactionsDir = "./transactions";
@@ -15,15 +18,23 @@ for (const file of files) {
   }
 }
 
-const categoryPolicy = JSON.parse(fs.readFileSync("/home/jakobedel/Projects/pdf_pipeline/category_policy.sample.json").toString());
+const policyFile = "/home/jakobedel/Projects/pdf_pipeline/category_policy.sample.json"
+const categoryPolicy = JSON.parse(fs.readFileSync(policyFile).toString());
 const model = buildModel(allTransactions, categoryPolicy)
 
-console.log(model);
-// const tokens = new Set();
-// for (const transaction of allTransactions) {
-//   for (const token of tokenize(transaction.opposing_name)) {
-//     tokens.add(token);
-//   }
-// }
 
-// console.log(tokens);
+const transactionsWithoutCategory = allTransactions.filter(transaction => {
+  return transaction.category_name === null;
+})
+
+// const rl = readline.createInterface({ input: process.stdin });
+let count = 1;
+for (const transaction of transactionsWithoutCategory) {
+  const merchant = transaction.opposing_name
+  const scores = classify(merchant, model);
+  console.log(`${count}. ${merchant}: ${scores[0].category}`);
+  // await rl.question("");
+  count++;
+}
+
+process.exit(0);
